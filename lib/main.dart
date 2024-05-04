b4350a8cde9aa206b222f4b530a0e5bc97b97198
anunglogin <akhmad.maknur@gmail.com>
2024-05-04 11:02:14 +0800

    feat: add font into widget text

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,81 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  void submitLogin(String value) {
+    print(value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: Text('Widget Dasar'),
+          title: Text('Contoh AppBar'),
           actions: [
             IconButton(
                 onPressed: () {
-                  print('saya di klik');
+                  submitLogin('search');
                 },
-                icon: Icon(Icons.search_rounded))
+                icon: Icon(Icons.search_rounded)),
+            IconButton(
+                onPressed: () {
+                  submitLogin('notification');
+                },
+                icon: Icon(Icons.notifications)),
           ],
         ),
-        body: Center(
-          child: Text('Hello World!'),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    alignment: Alignment.topRight,
+                    margin: EdgeInsets.all(5),
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text('Container 1'),
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    alignment: Alignment.topRight,
+                    margin: EdgeInsets.all(5),
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text('Container 2'),
+                  ),
+                )
+              ],
+            ),
+            Container(
+              child: Text(
+                'Container 3',
+                style: TextStyle(
+                    fontFamily: 'jaro',
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(10)),
+              height: 200,
+              width: double.infinity,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  submitLogin('submit');
+                },
+                child: Text('Submit'))
+          ],
         ),
       ),
     );
